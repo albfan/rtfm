9df5317508ab96738586cca4d3e8b9d264abd3e4
Christian Hergert <chergert@redhat.com>
2016-05-25 20:13:44 +0300

    platform: prototype some categorizing of dummy items

--- plugins/platform/platform.py
+++ plugins/platform/platform.py
@@ -37,23 +37,74 @@ _TITLES = collections.OrderedDict([
     ('platform:graphics',   _("Graphics")),
 ])
 
+_MAPPING = {
+    'dummy:Gst-1.0':             'platform:multimedia',
+    'dummy:GstAllocators-1.0':   'platform:multimedia',
+    'dummy:GstApp-1.0':          'platform:multimedia',
+    'dummy:GstAudio-1.0':        'platform:multimedia',
+    'dummy:GstBase-1.0':         'platform:multimedia',
+    'dummy:GstCheck-1.0':        'platform:multimedia',
+    'dummy:GstController-1.0':   'platform:multimedia',
+    'dummy:GstFft-1.0':          'platform:multimedia',
+    'dummy:GstGL-1.0':           'platform:multimedia',
+    'dummy:GstInsertBin-1.0':    'platform:multimedia',
+    'dummy:GstMpegts-1.0':       'platform:multimedia',
+    'dummy:GstNet-1.0':          'platform:multimedia',
+    'dummy:GstPbutils-1.0':      'platform:multimedia',
+    'dummy:GstPlayer-1.0':       'platform:multimedia',
+    'dummy:GstRtp-1.0':          'platform:multimedia',
+    'dummy:GstRtsp-1.0':         'platform:multimedia',
+    'dummy:GstSdp-1.0':          'platform:multimedia',
+    'dummy:GstTag-1.0':          'platform:multimedia',
+    'dummy:GstVideo-1.0':        'platform:multimedia',
+}
+
 class Category(Rtfm.Item):
     def __init__(self, category_id):
         super().__init__()
         self.props.id = category_id
         self.props.title = _TITLES[self.props.id]
 
+def findCategory(idstr):
+    return _MAPPING.get(idstr, None)
+
 class Categorizer(GObject.Object, Rtfm.Provider):
-    def do_load_children_async(self, path, collection, cancellable, callback, data):
+
+    # XXX: Add cleanup phase to provider
+
+    def do_cleanup(self, path, collection):
+        # If we are in the root path, we want to move the items
+        # info our category items.
         if path.is_empty():
+            # Create our injected category nodes
+            categories = {}
             for id in _TITLES.keys():
-                collection.add(Category(id))
+                category = Category(id)
+                categories[id] = category
 
-        task = Gio.Task.new(self, cancellable, callback)
-        task.return_boolean(True)
+            matching = []
 
-    def do_load_children_finish(self, task):
-        return task.propagate_boolean()
+            # Look through all the loaded items
+            for item in collection:
+                idstr = item.props.id
+                if idstr is None:
+                    continue
+
+                # If we find an item that is a dummy/gir node, then
+                # we want to possibly reorder it.
+                if idstr.startswith('dummy:') or idstr.startswith('gir:'):
+                    category_id = findCategory(idstr)
+                    if category_id in categories:
+                        categories[category_id].append(item)
+                        matching.append(item)
+
+            # Now remove any matching items
+            for item in matching:
+                collection.remove_item(item)
+
+            # Now add our categories
+            for category in categories.values():
+                collection.add(category)
 
     def do_extend_item_async(self, item, cancellable, callback, data):
         task = Gio.Task.new(self, cancellable, callback)
