c51deabb23b632059a1444bf261b31cc3afa4814
Christian Hergert <chergert@redhat.com>
2016-05-26 18:39:56 +0300

    platform: do some categorization

--- plugins/platform/platform.py
+++ plugins/platform/platform.py
@@ -31,37 +31,108 @@ from gi.repository import Rtfm
 
 _TITLES = collections.OrderedDict([
     ('platform:a11y',       _("Accessability")),
-    ('platform:crypto',     _("Cryptography")),
+    ('platform:core',       _("Core")),
+    ('platform:devel',      _("Developer Tools")),
+    ('platform:formats',    _("File Formats")),
     ('platform:multimedia', _("Multimedia")),
+    ('platform:languages',  _("Languages")),
     ('platform:networking', _("Networking")),
+    ('platform:os',         _("Operating System")),
+    ('platform:plugins',    _("Plugins")),
     ('platform:graphics',   _("Graphics")),
+    ('platform:security',   _("Security")),
+    ('platform:text',       _("Text and Spelling")),
+    ('platform:other',      _("Other")),
 ])
 
 _MAPPING = {
-    'dummy:Gst-1.0':             'platform:multimedia',
-    'dummy:GstAllocators-1.0':   'platform:multimedia',
-    'dummy:GstApp-1.0':          'platform:multimedia',
-    'dummy:GstAudio-1.0':        'platform:multimedia',
-    'dummy:GstBase-1.0':         'platform:multimedia',
-    'dummy:GstCheck-1.0':        'platform:multimedia',
-    'dummy:GstController-1.0':   'platform:multimedia',
-    'dummy:GstFft-1.0':          'platform:multimedia',
-    'dummy:GstGL-1.0':           'platform:multimedia',
-    'dummy:GstInsertBin-1.0':    'platform:multimedia',
-    'dummy:GstMpegts-1.0':       'platform:multimedia',
-    'dummy:GstNet-1.0':          'platform:multimedia',
-    'dummy:GstPbutils-1.0':      'platform:multimedia',
-    'dummy:GstPlayer-1.0':       'platform:multimedia',
-    'dummy:GstRtp-1.0':          'platform:multimedia',
-    'dummy:GstRtsp-1.0':         'platform:multimedia',
-    'dummy:GstSdp-1.0':          'platform:multimedia',
-    'dummy:GstTag-1.0':          'platform:multimedia',
-    'dummy:GstVideo-1.0':        'platform:multimedia',
+    'dummy:Atk-1.0':                 'platform:a11y',
+    'dummy:Atspi-2.0':               'platform:a11y',
 
-    'dummy:Gdk-3.0':             'platform:graphics',
-    'dummy:Gtk-3.0':             'platform:graphics',
-    'dummy:xlib-2.0':            'platform:graphics',
-    'dummy:xrandr-1.3':          'platform:graphics',
+    'dummy:GObject-2.0':             'platform:core',
+    'dummy:GLib-2.0':                'platform:core',
+    'dummy:GModule-2.0':             'platform:core',
+    'dummy:Gio-2.0':                 'platform:core',
+
+    'dummy:Ggit-1.0':                'platform:devel',
+    'dummy:Gladeui-2.0':             'platform:devel',
+    'dummy:AppStream-1.0':           'platform:devel',
+    'dummy:Vte-2.91':                'platform:devel',
+
+    'dummy:GtkSource-3.0':           'platform:text',
+    'dummy:GtkSpell-3.0':            'platform:text',
+    'dummy:Pango-1.0':               'platform:text',
+    'dummy:PangoCairo-1.0':          'platform:text',
+    'dummy:PangoXft-1.0':            'platform:text',
+    'dummy:PangoFT2-1.0':            'platform:text',
+
+    'dummy:Gst-1.0':                 'platform:multimedia',
+    'dummy:GstAllocators-1.0':       'platform:multimedia',
+    'dummy:GstApp-1.0':              'platform:multimedia',
+    'dummy:GstAudio-1.0':            'platform:multimedia',
+    'dummy:GstBase-1.0':             'platform:multimedia',
+    'dummy:GstCheck-1.0':            'platform:multimedia',
+    'dummy:GstController-1.0':       'platform:multimedia',
+    'dummy:GstFft-1.0':              'platform:multimedia',
+    'dummy:GstGL-1.0':               'platform:multimedia',
+    'dummy:GstInsertBin-1.0':        'platform:multimedia',
+    'dummy:GstMpegts-1.0':           'platform:multimedia',
+    'dummy:GstNet-1.0':              'platform:multimedia',
+    'dummy:GstPbutils-1.0':          'platform:multimedia',
+    'dummy:GstPlayer-1.0':           'platform:multimedia',
+    'dummy:GstRtp-1.0':              'platform:multimedia',
+    'dummy:GstRtsp-1.0':             'platform:multimedia',
+    'dummy:GstSdp-1.0':              'platform:multimedia',
+    'dummy:GstTag-1.0':              'platform:multimedia',
+    'dummy:GstVideo-1.0':            'platform:multimedia',
+
+    'dummy:Gdl-3':                   'platform:graphics',
+    'dummy:Gdk-3.0':                 'platform:graphics',
+    'dummy:GdkX11-3.0':              'platform:graphics',
+    'dummy:GdkPixbuf-2.0':           'platform:graphics',
+    'dummy:Gtk-3.0':                 'platform:graphics',
+    'dummy:xlib-2.0':                'platform:graphics',
+    'dummy:xrandr-1.3':              'platform:graphics',
+    'dummy:xft-2.0':                 'platform:graphics',
+    'dummy:xfixes-4.0':              'platform:graphics',
+    'dummy:freetype2-2.0':           'platform:graphics',
+    'dummy:fontconfig-2.0':          'platform:graphics',
+    'dummy:cairo-1.0':               'platform:graphics',
+    'dummy:GL-1.0':                  'platform:graphics',
+
+    'dummy:DBus-1.0':                'platform:networking',
+    'dummy:DBusGLib-1.0':            'platform:networking',
+    'dummy:Soup-2.4':                'platform:networking',
+    'dummy:SoupGNOME-2.4':           'platform:networking',
+    'dummy:WebKit2-4.0':             'platform:networking',
+    'dummy:WebKit2WebExtension-4.0': 'platform:networking',
+    'dummy:TelepathyGLib-0.12':      'platform:networking',
+
+    'dummy:JavaScriptCore-4.0':      'platform:languages',
+    'dummy:GIRepository-2.0':        'platform:languages',
+    'dummy:Gee-0.8':                 'platform:languages',
+
+    'dummy:Gck-1':                   'platform:security',
+    'dummy:CryptUI-0.0':             'platform:security',
+    'dummy:Secret-1':                'platform:security',
+    'dummy:Polkit-1.0':              'platform:security',
+    'dummy:PolkitAgent-1.0':         'platform:security',
+    'dummy:Gcr-3':                   'platform:security',
+    'dummy:GcrUi-3':                 'platform:security',
+
+    'dummy:Json-1.0':                'platform:formats',
+    'dummy:libxml2-2.0':             'platform:formats',
+    'dummy:GCab-1.0':                'platform:formats',
+
+    'dummy:Peas-1.0':                'platform:plugins',
+    'dummy:PeasGtk-1.0':             'platform:plugins',
+
+    'dummy:GSystem-1.0':             'platform:os',
+    'dummy:OSTree-1.0':              'platform:os',
+    'dummy:XdgApp-1.0':              'platform:os',
+    'dummy:Flatpak-1.0':             'platform:os',
+    'dummy:win32-1.0':               'platform:os',
+    'dummy:GUdev-1.0':               'platform:os',
 }
 
 class Category(Rtfm.Item):
@@ -71,11 +142,9 @@ class Category(Rtfm.Item):
         self.props.title = _TITLES[self.props.id]
 
 def findCategory(idstr):
-    return _MAPPING.get(idstr, None)
+    return _MAPPING.get(idstr, 'platform:other')
 
 class Categorizer(GObject.Object, Rtfm.Provider):
-
-    # XXX: Add cleanup phase to provider
 
     def do_postprocess(self, collection):
         path = collection.get_path()
@@ -97,11 +166,14 @@ class Categorizer(GObject.Object, Rtfm.Provider):
                 collection.remove_item(match)
             if categories:
                 categories = list(categories.values())
-                categories.sort(key=lambda x: x.get_title())
+                categories.sort(key=lambda x: x.get_title(), reverse=True)
                 for category in categories:
                     collection.prepend(category)
 
     def do_load_item(self, id):
+        # TODO: Not really sufficient for loading from an item when restoring
+        #       a previous item at startup. We need to ensure that the first
+        #       layer is loaded and that we have cached the items.
         if id in _TITLES:
             return Category(id)
         return None
