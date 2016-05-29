1418e34f97bce02ddafc16f264921e30c6c1fd6e
Christian Hergert <chergert@redhat.com>
2016-05-29 16:27:50 +0300

    gir: add libxml dependency and more stubs
    We'll need libxml for pretty much all backends, so we can just take it as
    a system dependency.

--- plugins/gobject-introspection/rtfm-gir-compiler.c
+++ plugins/gobject-introspection/rtfm-gir-compiler.c
@@ -21,23 +21,80 @@
 #endif
 
 #include <glib/gi18n.h>
+#include <libxml/xmlreader.h>
 #include <rtfm.h>
 #include <stdlib.h>
+
+static gboolean
+rtfm_gir_compiler_compile (GFile   *in_file,
+                           GFile   *out_file,
+                           GError **error)
+{
+  g_assert (G_IS_FILE (in_file));
+  g_assert (G_IS_FILE (out_file));
+  g_assert (error != NULL);
+
+  return TRUE;
+}
 
 gint
 main (gint   argc,
       gchar *argv[])
 {
-  g_autoptr(GOptionContext) context = NULL;
-  g_autoptr(GError) error = NULL;
+  g_autofree gchar *library_version = NULL;
   GOptionEntry entries[] = {
+    { "library-version", 0, 0, G_OPTION_ARG_STRING, &library_version,
+      N_("The version of the library or program .gir"),
+      N_("VERSION") },
     { NULL }
   };
 
-  context = g_option_context_new (_("- generate documentation for a .gir file"));
+  g_autoptr(GOptionContext) context = NULL;
+  g_autoptr(GError) error = NULL;
+  g_autoptr(GFile) file = NULL;
+  g_autoptr(GFile) parent = NULL;
+  g_autoptr(GFile) outfile = NULL;
+  g_autofree gchar *basename = NULL;
+  g_autofree gchar *outbasename = NULL;
+
+  context = g_option_context_new (_("GIR_FILE - generate documentation for a .gir file"));
   g_option_context_add_main_entries (context, entries, GETTEXT_PACKAGE);
 
   if (!g_option_context_parse (context, &argc, &argv, &error))
+    {
+      g_printerr ("%s\n", error->message);
+      return EXIT_FAILURE;
+    }
+
+  if (argc != 2)
+    {
+      g_autofree gchar *help = g_option_context_get_help (context, TRUE, NULL);
+      g_printerr ("%s\n", help);
+      return EXIT_FAILURE;
+    }
+
+  if (!g_str_has_suffix (argv[1], ".gir"))
+    {
+      g_printerr ("\"%s\" does not look like a .gir file.\n", argv[1]);
+      return EXIT_FAILURE;
+    }
+
+  file = g_file_new_for_commandline_arg (argv[1]);
+
+  if (!g_file_query_exists (file, NULL) ||
+      g_file_query_file_type (file, 0, NULL) != G_FILE_TYPE_REGULAR)
+    {
+      g_autofree gchar *filename = g_file_get_path (file);
+      g_printerr ("Invalid file: %s\n", filename);
+      return EXIT_FAILURE;
+    }
+
+  parent = g_file_get_parent (file);
+  basename = g_file_get_basename (file);
+  outbasename = g_strdup_printf ("%s.rtfm", basename);
+  outfile = g_file_get_child (parent, outbasename);
+
+  if (!rtfm_gir_compiler_compile (file, outfile, &error))
     {
       g_printerr ("%s\n", error->message);
       return EXIT_FAILURE;
