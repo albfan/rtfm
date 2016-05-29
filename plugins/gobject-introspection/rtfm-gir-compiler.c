b14111cbb63b120ee3e12e917f408b0398a678b3
Christian Hergert <chergert@redhat.com>
2016-05-29 16:00:05 +0300

    gir: stub out rtfm-gir-compiler
    The goal of this program is to parse a .gir file and generate an mmap()able
    file containing a GVariant which we can read at runtime to browse docs.
    The RtfmGirProvider will read from these files. How we get access to them
    is still a bit up in the air. We can generate them locally or we can get
    access to them via some online mechanism such as developer.gnome.org.

---
+++ plugins/gobject-introspection/rtfm-gir-compiler.c
@@ -0,0 +1,47 @@
+/* rtfm-gir-compiler.c
+ *
+ * Copyright (C) 2016 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <glib/gi18n.h>
+#include <rtfm.h>
+#include <stdlib.h>
+
+gint
+main (gint   argc,
+      gchar *argv[])
+{
+  g_autoptr(GOptionContext) context = NULL;
+  g_autoptr(GError) error = NULL;
+  GOptionEntry entries[] = {
+    { NULL }
+  };
+
+  context = g_option_context_new (_("- generate documentation for a .gir file"));
+  g_option_context_add_main_entries (context, entries, GETTEXT_PACKAGE);
+
+  if (!g_option_context_parse (context, &argc, &argv, &error))
+    {
+      g_printerr ("%s\n", error->message);
+      return EXIT_FAILURE;
+    }
+
+  return EXIT_SUCCESS;
+}
