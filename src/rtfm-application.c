32831f36b6bbddcd2d1c5fbcc9a3c0edf27d1057
Christian Hergert <chergert@redhat.com>
2016-06-11 15:09:09 -0700

    app: add -k <keyword> command line option
    This will print a list of search results similar to `man -k <keyword>`.
    However, since search indexes load asynchronously at startup, it doesn't
    show any results yet.
    
    We need to add plumbing (a single GTask) to wait on for search indexes
    to load.

--- src/rtfm-application.c
+++ src/rtfm-application.c
@@ -18,7 +18,17 @@
 
 #define G_LOG_DOMAIN "rtfm-application"
 
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <glib/gi18n.h>
+
 #include "rtfm-application.h"
+#include "rtfm-library.h"
+#include "rtfm-search-result.h"
+#include "rtfm-search-results.h"
+#include "rtfm-search-settings.h"
 #include "rtfm-window.h"
 
 struct _RtfmApplication
@@ -60,11 +70,120 @@ rtfm_application_activate (GApplication *app)
 }
 
 static void
+rtfm_application_search_by_keyword_cb (GObject      *object,
+                                       GAsyncResult *result,
+                                       gpointer      user_data)
+{
+  RtfmLibrary *library = (RtfmLibrary *)object;
+  GMainLoop *main_loop = user_data;
+  g_autoptr(GError) error = NULL;
+
+  g_assert (RTFM_IS_LIBRARY (library));
+
+  if (!rtfm_library_search_finish (library, result, &error))
+    g_printerr ("%s\n", error->message);
+
+  g_main_loop_quit (main_loop);
+}
+
+static void
+rtfm_application_search_by_keyword (RtfmApplication *self,
+                                    const gchar     *keyword)
+{
+  g_autoptr(GMainLoop) main_loop = NULL;
+  g_autoptr(RtfmSearchResults) search_results = NULL;
+  g_autoptr(RtfmSearchSettings) search_settings = NULL;
+  RtfmLibrary *library;
+  guint i;
+  guint n_items;
+
+  g_assert (RTFM_IS_APPLICATION (self));
+  g_assert (keyword != NULL);
+
+  main_loop = g_main_loop_new (NULL, FALSE);
+  library = rtfm_library_get_default ();
+
+  search_settings = rtfm_search_settings_new ();
+  rtfm_search_settings_set_search_text (search_settings, keyword);
+
+  search_results = rtfm_search_results_new (0);
+
+  rtfm_library_search_async (library,
+                             search_settings,
+                             search_results,
+                             NULL,
+                             rtfm_application_search_by_keyword_cb,
+                             main_loop);
+
+  g_main_loop_run (main_loop);
+
+  n_items = g_list_model_get_n_items (G_LIST_MODEL (search_results));
+
+  for (i = 0; i < n_items; i++)
+    {
+      g_autoptr(RtfmSearchResult) result = NULL;
+      const gchar *text;
+      gfloat score;
+
+      result = g_list_model_get_item (G_LIST_MODEL (search_results), i);
+      text = rtfm_search_result_get_text (result);
+      score = rtfm_search_result_get_score (result);
+
+      g_print ("%s (%f)\n", text, score);
+    }
+
+  g_print ("%u results\n", n_items);
+}
+
+static gboolean
+rtfm_application_local_command_line (GApplication   *application,
+                                     gchar        ***arguments,
+                                     gint           *exit_status)
+{
+  g_autoptr(GOptionContext) context = NULL;
+  g_autoptr(GError) error = NULL;
+  g_autofree gchar *keyword = NULL;
+  gint argc;
+
+  const GOptionEntry entries[] = {
+    { "keyword", 'k', 0, G_OPTION_ARG_STRING, &keyword },
+    { NULL }
+  };
+
+  g_assert (G_IS_APPLICATION (application));
+  g_assert (arguments != NULL);
+  g_assert (exit_status != NULL);
+
+  argc = g_strv_length (*arguments);
+
+  context = g_option_context_new (_("- Read the Fine Manual"));
+  g_option_context_set_help_enabled (context, FALSE);
+  g_option_context_set_ignore_unknown_options (context, TRUE);
+  g_option_context_add_main_entries (context, entries, GETTEXT_PACKAGE);
+
+  if (!g_option_context_parse (context, &argc, arguments, &error))
+    {
+      g_printerr ("%s\n", error->message);
+      return TRUE;
+    }
+
+  if (keyword != NULL)
+    {
+      rtfm_application_search_by_keyword (RTFM_APPLICATION (application), keyword);
+      *exit_status = 0;
+      return TRUE;
+    }
+
+  return G_APPLICATION_CLASS (rtfm_application_parent_class)->local_command_line (application, arguments, exit_status);
+}
+
+static void
 rtfm_application_class_init (RtfmApplicationClass *klass)
 {
   GApplicationClass *app_class = G_APPLICATION_CLASS (klass);
 
   app_class->activate = rtfm_application_activate;
+  app_class->local_command_line = rtfm_application_local_command_line;
 }
 
 static void
